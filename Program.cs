5de0a108458f7e7fbe2499f80977c18fd391002f
uozawo <230103594+uozawo@users.noreply.github.com>
2026-05-25 02:10:00 +0000

    add empty program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DiplomaDefense.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("привіт");
+        }
+    }
+}
